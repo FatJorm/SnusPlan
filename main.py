136cca3594bb56ce96383436630d7599abfcbebd
Johan Steen <j_persson@hotmail.com>
2023-08-18 12:22:13 +0200

    Change back to a simplified file structure

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.clock import Clock
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.anchorlayout import AnchorLayout
-from kivy.core.window import Window  # <-- Add this import
 
-from Plan.Plan import Plan
+from Plan import Plan
 from datetime import datetime
 
 
