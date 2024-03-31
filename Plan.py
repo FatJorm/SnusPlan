41eb33da319b2c2edb227999f6a21c1578829c00
Johan Steen <j_persson@hotmail.com>
2024-03-31 10:20:58 +0200

    New version working

--- Plan.py
+++ Plan.py
@@ -10,12 +10,10 @@ class SnusTime:
     @property
     def time(self):
         return self._time
+
     @property
     def time_string(self):
         return self._time.strftime("%d/%m %H:%M")
-
-    def __radd__(self, day_plan):
-        day_plan.append(self)
 
     def next(self, time_diff):
         time = self._time + timedelta(minutes=time_diff)
@@ -26,81 +24,80 @@ class DayPlan:
     start_time = datetime(year=1970, month=1, day=1, hour=7, minute=0, second=0)
     end_time = datetime(year=1970, month=1, day=1, hour=21, minute=0, second=0)
 
-    def __init__(self, plan_date, dose_today):
-        self._start_time = self._get_date_time(plan_date, self.start_time)
-        self._end_time = self._get_date_time(plan_date, self.end_time)
-        self._min_between = self._get_min_between(dose_today)
-        self._day_plan = self._get_plan(dose_today)
+    def __init__(self, plan_date, day_dose):
+        self._plan_date = plan_date
+        self._day_dose = day_dose
+        self._day_plan = self._get_day_plan()
 
-    @property
-    def done(self):
-        return not self._day_plan
-
-    @staticmethod
-    def _get_date_time(date, time):
-        return datetime(year=date.year,
-                        month=date.month,
-                        day=date.day,
+    def _get_date_time(self, time):
+        return datetime(year=self._plan_date.year,
+                        month=self._plan_date.month,
+                        day=self._plan_date.day,
                         hour=time.hour,
                         minute=time.minute,
                         second=time.second)
 
-    def _get_min_between(self, dose_today):
-        day_in_min = self._get_snus_day_in_min(dose_today, self._start_time, self._end_time)
-        return int(day_in_min/(dose_today-1))
+    def _get_delta_min(self):
+        day_start = self._get_date_time(self.start_time)
+        day_end = self._get_date_time(self.end_time)
+        day_in_min = self._get_day_in_min(self._day_dose, day_start, day_end)
+        return int(day_in_min/(self._day_dose-1))
 
     @staticmethod
-    def _get_snus_day_in_min(dose_today, start_time, end_time):
+    def _get_day_in_min(dose_today, start_time, end_time):
         day_in_min = (end_time.hour - start_time.hour) * 60 + (end_time.minute - start_time.minute)
         if dose_today > 1:
             return int(day_in_min - (day_in_min/dose_today) * 0.5)
         else:
             return day_in_min
 
-    def _get_plan(self, dose_today):
-        day_plan = []
-        snus_time = SnusTime(self._start_time)
-        day_plan.append(snus_time)
-        if dose_today > 1:
-            for i in range(dose_today-1):
-                snus_time = snus_time.next(self._min_between)
-                day_plan.append(snus_time)
-        day_plan.reverse()
-        return self._remove_passed(day_plan)
+    def _get_day_plan(self):
+        plan = []
+        day_start = self._get_date_time(self.start_time)
+        delta_min = self._get_delta_min()
+        snus_time = SnusTime(day_start)
+        plan.append(snus_time)
+        if self._day_dose > 1:
+            for i in range(self._day_dose-1):
+                snus_time = snus_time.next(delta_min)
+                plan.append(snus_time)
+        plan.reverse()
+        return plan
 
-    @staticmethod
-    def _remove_passed(daily_plan):
-        out_l = []
-        now = datetime.now()
-        for time in daily_plan:
-            if time.time > now:
-                out_l.append(time)
-        return out_l
+    def next_day(self):
+        self._plan_date = datetime.now() + timedelta(days=1)
+        self._day_plan = self._get_day_plan()
 
+    def pop(self):
+        return self._day_plan.pop()
 
+    @property
+    def date(self):
+        return self._plan_date
+
+    @date.setter
+    def date(self, plan_date):
+        self._plan_date = plan_date
+        self._day_plan = self._get_day_plan()
+
+    @property
     def next(self):
-        self._day_plan.pop()
+        return self._day_plan[-1]
 
-    def get_next_time(self):
-        return self._day_plan[-1].time
+    @property
+    def dose(self):
+        return self._day_dose
 
-    def get_next_time_string(self):
-        return self._day_plan[-1].time_string
+    @dose.setter
+    def dose(self, day_dose):
+        self._day_dose = day_dose
 
-    def get_next_day(self, dose):
-        plan_date = self._start_time + timedelta(days=1)
-        return DayPlan(plan_date, dose)
+    @property
+    def done(self):
+        return not self._day_plan
 
     def __str__(self):
         string = ""
         for snus_time in self._day_plan:
             string += f"{snus_time.time_string}\n"
         return string
-
-
-if __name__ == '__main__':
-    dose = 7
-    day_plan = DayPlan(dose)
-    print(day_plan)
-    day_plan.next()
-    print(day_plan)
--- main.py
+++ main.py
@@ -4,16 +4,16 @@ from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.clock import Clock
 from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
 from Plan import DayPlan
 from pathlib import Path
-
+import pickle
 from datetime import datetime, date
-import copy
 
 
 # Set the desired window size
-#scale = 0.5
-#Window.size = (700*scale, 1280*scale)  # <-- For example, this sets the window size to 500x700 pixels.
+scale = 0.5
+Window.size = (700*scale, 1280*scale)  # <-- For example, this sets the window size to 500x700 pixels.
 
 
 class MainWindow(FloatLayout):
@@ -25,51 +25,55 @@ class MainWindow(FloatLayout):
         # Center container for main_btn and next_snus_label
         center_box = BoxLayout(orientation='vertical', spacing=40, size_hint=(0.5, 0.2), pos_hint={'center_x': 0.5, 'center_y': 0.5})
         self.main_btn = Button(text="SNUS", on_release=self.push_main_btn, size_hint=(1, 0.95))
+        center_box.add_widget(self.main_btn)
+        self.add_widget(center_box)
 
-         # Adjustments box with decrease button, label, and increase button
-        adjustments_box = BoxLayout(orientation='horizontal', spacing=10, size_hint=(1, None), height=50)
-        self.dec_btn = Button(text="-", on_release=self.decrease_value)
-        self.inc_btn = Button(text="+", on_release=self.increase_value)
-        self.value_label = Label(text=str(self.main_app.current_dose), size_hint=(1, 1), halign='center', valign='center')
-
+       # Adjustments box in the upper right corner
+        adjustments_box = BoxLayout(orientation='horizontal', spacing=10, size_hint=(None, None), size=(200, 50), pos_hint={'right': 1, 'top': 1})
+        self.dec_btn = Button(text="-", on_release=self.decrease_value,
+                              background_normal='', background_color=(0, 0, 0, 1),
+                              color=(1, 1, 1, 1))  # White text
+        self.inc_btn = Button(text="+", on_release=self.increase_value,
+                              background_normal='', background_color=(0, 0, 0, 1),
+                              color=(1, 1, 1, 1))  # White text
+        self.value_label = Label(text=str(self.main_app.plan.dose), size_hint=(None, None), size=(60, 50))
         adjustments_box.add_widget(self.dec_btn)
         adjustments_box.add_widget(self.value_label)
         adjustments_box.add_widget(self.inc_btn)
 
-        center_box.add_widget(adjustments_box)
-        center_box.add_widget(self.main_btn)
+        # Adding adjustments box directly to the MainWindow layout
+        self.add_widget(adjustments_box)
 
+        # Configuration for the next_snus_label remains the same
         self.next_snus_label = Label(
-                                    text=f"Next snus: {self.plan.get_next_time_string()}",
+                                    text=f"Next snus: {self.plan.next.time_string}",
                                     color=(0, 1, 0, 1),
-                                    size_hint=(1, 0.05),  # This ensures the Label takes up the full width of the BoxLayout
-                                    halign='center',   # Horizontally centers the text inside the label
-                                    valign='center',   # Vertically centers the text inside the label
+                                    size_hint=(1, 0.05),
+                                    halign='center',
+                                    valign='center',
                                     )
-
         center_box.add_widget(self.next_snus_label)
 
-        self.add_widget(center_box)
-
+        # Update main button and scheduling event remain unchanged
         self.update_main_button()
         self._event = Clock.schedule_interval(self.update_main_button, 1)
 
     def increase_value(self, instance):
         # Increase the value and update the label
-        self.main_app.current_dose += 1  # Assuming `current_dose` is an integer
-        self.value_label.text = str(self.main_app.current_dose)
-        self._save_current_dose(self.main_app.current_dose)
+        self.main_app.plan.dose += 1  # Assuming `current_dose` is an integer
+        self.value_label.text = str(self.main_app.plan.dose)
+        self._save_state(self.main_app.plan)
 
     def decrease_value(self, instance):
         # Decrease the value and update the label, ensuring it doesn't go below a minimum value (e.g., 0)
-        if self.main_app.current_dose > 0:
-            self.main_app.current_dose -= 1
-        self.value_label.text = str(self.main_app.current_dose)
-        self._save_current_dose(self.main_app.current_dose)
+        if self.main_app.plan.dose > 0:
+            self.main_app.plan.dose -= 1
+        self.value_label.text = str(self.main_app.plan.dose)
+        self._save_state(self.main_app.plan)
 
-    def _save_current_dose(self, dose):
-        with open(self.main_app.state_storage, 'w+') as f:
-            f.write(str(dose))
+    def _save_state(self, day_plan):
+        with open(self.main_app.state_storage, 'wb') as f:
+            pickle.dump(day_plan, f)
 
     def update_main_button(self, *args):
         if self.time_for_next():
@@ -84,7 +88,7 @@ class MainWindow(FloatLayout):
     def time_for_next(self):
         now = datetime.now()
         if not self.plan.done:
-            next_time = self.plan.get_next_time()
+            next_time = self.plan.next.time
             if next_time and now > next_time:
                 return True
             else:
@@ -93,11 +97,10 @@ class MainWindow(FloatLayout):
             return False
 
     def push_main_btn(self, instance):
-        self.plan.next()
+        self.plan.pop()
         if self.plan.done:
-            self.main_app.plan = self.plan.get_next_day(self.main_app.current_dose)
-        print(f'{self.plan}')
-        #self.next_snus_label.text = f"Next snus: {self.plan.get_next_time_string()}"
+            self.plan.next_day()
+        self._save_state(self.main_app.plan)
         self.main_app.root_window()
 
     @staticmethod
@@ -112,17 +115,20 @@ class SnusManagerApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.state_storage = Path(r"state_storage.pkl")
-        self.current_dose = self._get_current_dose()
-        self.plan = DayPlan(date.today(), self.current_dose)
+        self.plan = self._get_day_plan()
 
-    def _get_current_dose(self):
+    def _get_day_plan(self):
         if self.state_storage.exists():
-            with open(self.state_storage, 'r') as f:
-                value = int(f.read())
-                if value != '':
-                    return int(value)
+            return self._load_plan()
         else:
-            return 7
+            return DayPlan(date.today(), 5)
+
+    def _load_plan(self):
+        with open(self.state_storage, 'rb') as f:
+            plan = pickle.load(f)
+        if plan.date < datetime.now():
+            plan.date = datetime.now()
+        return plan
 
     def build(self):
         self.title = 'Snus Manager'
